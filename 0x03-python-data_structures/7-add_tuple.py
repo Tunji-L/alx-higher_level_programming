91842095955a2dabbea1edb29896b224a616b5f9
Tunji-L <oyetunjilukman@gmial.com>
2022-06-04 14:16:59 +0000

    Tuples addition

---
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+def add_tuple(tuple_a=(), tuple_b=()):
+    a = len(tuple_a)
+    b = len(tuple_b)
+
+    if a == 0:
+        tuple_a = (0, 0)
+    elif a == 1:
+        tuple_a = (tuple_a[0], 0)
+    else:
+        tuple_a = tuple_a[0: 2]
+
+    if b == 0:
+        tuple_b = (0, 0)
+    elif b == 1:
+        tuple_b = (tuple_b[0], 0)
+    else:
+        tuple_b = tuple_b[0: 2]
+
+    new_tuple = (tuple_a[0] + tuple_b[0], tuple_a[1] + tuple_b[1])
+
+    return new_tuple
