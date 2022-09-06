5daa9d99c9a87c78a920f51c8d2a798747bd3c0e
Tunji-L <oyetunjilukman@gmail.com>
2022-09-06 03:56:30 +0000

    feat: A script that prints x times 'C is fun'

---
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+const myArgs = process.argv.slice(2);
+const occurence = Number(myArgs[0]);
+if (occurence) {
+  for (let i; i <= occurence; i++) {
+    console.log('C is fun');
+  }
+} else {
+  console.log('Missing number of occurrences');
+}
