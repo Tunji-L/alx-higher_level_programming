f5325907443c3dd1cbb2885b7e7c6ff7ac21e131
Tunji-L <oyetunjilukman@gmail.com>
2022-09-09 09:54:58 +0000

    fix: replace <= with <

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -2,7 +2,7 @@
 const myArgs = process.argv.slice(2);
 const occurence = Number(myArgs[0]);
 if (occurence) {
-  for (let i = 0; i <= occurence; i++) {
+  for (let i = 0; i < occurence; i++) {
     console.log('C is fun');
   }
 } else {
