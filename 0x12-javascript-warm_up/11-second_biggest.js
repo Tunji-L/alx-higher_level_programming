c0416552cae77cc76fa7c9c200d4b216897c3028
Tunji-L <oyetunjilukman@gmail.com>
2022-09-09 11:15:55 +0000

    fix: Add function to the sort method

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,9 +1,9 @@
 #!/usr/bin/node
-const myArgs = process.argv.slice(2);
+const myArgs = process.argv.slice(2).map(occ => Number(occ));
 if (!myArgs[0] || myArgs.length === 1) {
   console.log(0);
 } else {
-  myArgs.sort();
+  myArgs.sort((a, b) => a - b);
   myArgs.reverse();
   console.log(myArgs[1]);
 }
